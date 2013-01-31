7ad7f60180bfef00d17a1dc1c409fc3012531b88
Jeremiah Hemphill <jeremiah@cloudspace.com>
2013-01-31 08:25:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyBackNine::Application.config.secret_token = '84e76100d19482591b8d65de10abe7abd6c3781072486e64949ba8f28263686be7116f44b50f8492506c1b48dc071ed8508905f4efefe62eb24b51381e48a13b'
